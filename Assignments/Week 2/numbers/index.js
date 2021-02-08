d9d01a881a11099f4fd3f8796307c24b41043a6c
GeraldRyan <gerald.ryan40@protonmail.com>
2021-02-08 16:48:25 -0600

    Got numeral JS library mostly working

--- Assignments/Week 2/numbers/index.js
+++ Assignments/Week 2/numbers/index.js
@@ -1,8 +1,54 @@
-// document.getElementById("one").addEventListener()
+document.getElementById("zero").addEventListener('click', function () { numberClicked(0) })
+document.getElementById("one").addEventListener('click', function () { numberClicked(1) })
+document.getElementById("two").addEventListener('click', function () { numberClicked(2) })
+document.getElementById("three").addEventListener('click', function () { numberClicked(3) })
+document.getElementById("four").addEventListener('click', function () { numberClicked(4) })
+document.getElementById("five").addEventListener('click', function () { numberClicked(5) })
+document.getElementById("six").addEventListener('click', function () { numberClicked(6) })
+document.getElementById("seven").addEventListener('click', function () { numberClicked(7) })
+document.getElementById("eight").addEventListener('click', function () { numberClicked(8) })
+document.getElementById("nine").addEventListener('click', function () { numberClicked(9) })
+document.getElementById("double-zero").addEventListener('click', function () { doubleZeroClicked() })
+
+document.getElementById("six").addEventListener('click', function () { numberClicked(6) })
+document.getElementById("six").addEventListener('click', function () { numberClicked(6) })
+document.getElementById("six").addEventListener('click', function () { numberClicked(6) })
+document.getElementById("six").addEventListener('click', function () { numberClicked(6) })
+document.getElementById("six").addEventListener('click', function () { numberClicked(6) })
+document.getElementById("six").addEventListener('click', function () { numberClicked(6) })
+document.getElementById("six").addEventListener('click', function () { numberClicked(6) })
+
+
+document.getElementById("equals-sign").addEventListener('click', function () { computeFigure() })
+
+// document.getElementById("output-screen").addEventListener('click', function () { doSomethingExtraordinary() })
 
 
 
+function computeFigure()
+{
+  console.log("Equals sign pressed")
+  value = 88765.215
+  outputToScreen(value)
 
+}
+
+function outputToScreen(value)
+{
+  // value = numeral(value).format('0.00')
+  document.getElementById("output-screen").innerText = numeral(value).format('0,000.00')
+
+}
+
+function numberClicked(number)
+{
+  console.log(number);
+}
+
+function doubleZeroClicked()
+{
+  console.log(0, 0)
+}
 
 function addXNumbers(...zargs)
 {
