fe9428c33235d26de46290e5c2803e46ebb8d778
GeraldRyan <gerald.ryan40@protonmail.com>
2021-02-08 21:09:49 -0600

    Added log and power functionality and e and pi

--- Assignments/Week 2/numbers/index.js
+++ Assignments/Week 2/numbers/index.js
@@ -1,3 +1,5 @@
+// Can I do with PUG or other PreProcessor, ??????????????????????????????????????????
+//????????????????????????????????????????????????????????????????????????????????????
 document.getElementById("zero").addEventListener('click', function () { numberClicked(0) })
 document.body.addEventListener('keydown', function (e) { if (e.code == "Digit0" || e.code == "Numpad0") numberClicked(0) })
 document.getElementById("one").addEventListener('click', function () { numberClicked(1) })
@@ -18,38 +20,38 @@ document.getElementById("eight").addEventListener('click', function () { numberC
 document.body.addEventListener('keydown', function (e) { if (e.code == "Digit8" || e.code == "Numpad8") numberClicked(8) })
 document.getElementById("nine").addEventListener('click', function () { numberClicked(9) })
 document.body.addEventListener('keydown', function (e) { if (e.code == "Digit9" || e.code == "Numpad9") numberClicked(9) })
-
-
-
-document.body.addEventListener('keydown', (e) => { console.log(e.code) })
-
-
-
+// document.body.addEventListener('keydown', (e) => { console.log(e.code) }) // Great for debugging
 document.getElementById("decimal-point").addEventListener('click', function () { decimalPointClicked() })
 document.body.addEventListener('keydown', function (e) { if (e.code == "NumpadDecimal" || e.code == "Period") decimalPointClicked() })
-
-document.getElementById("double-zero").addEventListener('click', function () { doubleZeroClicked() })
-
+document.getElementById("power-btn").addEventListener('click', function () { powerClicked() })
+document.getElementById("log-btn").addEventListener('click', function () { logClicked() })
 document.getElementById("clear-btn").addEventListener('click', function () { startOver() })
 document.body.addEventListener('keydown', function (e) { if (e.code == "KeyC") startOver() })
-
 document.getElementById("divide-btn").addEventListener('click', function () { functionButtonClicked("/") })
 document.body.addEventListener('keydown', function (e) { if (e.code == "NumpadDivide") functionButtonClicked("/") })
-
-
 document.getElementById("multiply-btn").addEventListener('click', function () { functionButtonClicked("*") })
 document.body.addEventListener('keydown', function (e) { if (e.code == "NumpadMultiply") functionButtonClicked("*") })
-
 document.getElementById("add-btn").addEventListener('click', function () { functionButtonClicked("+") })
 document.body.addEventListener('keydown', function (e) { if (e.code == "NumpadAdd") functionButtonClicked("+") })
-
 document.getElementById("subtract-btn").addEventListener('click', function () { functionButtonClicked("-") })
 document.body.addEventListener('keydown', function (e) { if (e.code == "NumpadSubtract") functionButtonClicked("-") })
-
 document.getElementById("equals-sign").addEventListener('click', function () { computeFigure() })
 document.body.addEventListener('keydown', function (e) { if (e.code == "NumpadEnter" || e.code == "Equal") computeFigure() })
+// Can I do with PUG or other PreProcessor, ??????????????????????????????????????????
+//????????????????????????????????????????????????????????????????????????????????????
 
 
+//Extras
+document.getElementById("pi-btn").addEventListener('click', function () { numberClicked(3.142857) })
+document.getElementById("e-btn").addEventListener('click', function () { numberClicked(2.71828) })
+
+document.getElementById("rr-btn").addEventListener('click', function () { rr() })
+
+
+
+
+// Can I do without global variables, or in class based architecture?????????????????
+//???????????????????????????????????????????????????????????????????????????????????? 
 let buffer = 0
 let nCount = 0
 let dCount = -1
@@ -59,10 +61,16 @@ let bDecimalPointJustClicked = false
 let bDecimalPointInBuffer = false
 let currentOp = null
 let bComputedNumberOnScreen = false
+let bPowerButtonPressed = false
+let bNegativeNumber = false
+// Can I do without global variables, or in class based architecture?????????????????
+//????????????????????????????????????????????????????????????????????????????????????
+
 
 function numberClicked(number)
 {
-  if (bComputedNumberOnScreen == true && currentOp == null){
+  if (bComputedNumberOnScreen == true && currentOp == null)
+  {
     startOver() // Assume they want a new calculation without having to explicitly clear screen
   }
   if (bDecimalPointJustClicked == false && bDecimalPointInBuffer == false)
@@ -75,16 +83,11 @@ function numberClicked(number)
     buffer = math.round(buffer + number * Math.pow(10, dCount), (-dCount))
     dCount--
   }
+  if (bNegativeNumber){
+    buffer = -Math.abs(buffer)
+  } 
   outputToScreen(buffer)
 }
-
-// function buildInts(buffer, newNumber){
-
-// }
-
-// function buildDecimals(buffer, newNumber){
-
-// }
 
 function decimalPointClicked()
 {
@@ -98,6 +101,15 @@ function decimalPointClicked()
 
 function functionButtonClicked(button)
 {
+  // console.log(bPowerButtonPressed, button)
+  if (button == "-" && bPowerButtonPressed)
+  {
+    // They just want to make the number negative
+    //  buffer 
+    bNegativeNumber = true
+    console.log("We're here")
+    return;
+  }
   console.log(button, " clicked")
   nCount = 0
   dCount = -1
@@ -115,17 +127,43 @@ function functionButtonClicked(button)
   currentOp = button
 }
 
+function powerClicked()
+{
+  console.log("Power clicked")
+  currentOp = "**"
+  nCount = 0
+  dCount = -1
+  bDecimalPointInBuffer = false
+  bDecimalPointJustClicked = false
+  arg1 = buffer
+  buffer = 0
+  bPowerButtonPressed = true
+}
+
+function logClicked(){
+  currentOp = "log"
+  nCount = 0
+  dCount = -1
+  // bDecimalPointInBuffer = false
+  // bDecimalPointJustClicked = false
+  arg1 = buffer
+  // buffer = 0
+  computeFigure()
+}
+
 function startOver()
 {
   arg1 = 0
   bComputedNumberOnScreen = false
   flushTempValues()
   outputToScreen(0)
+  powerClicked = false
+
 }
 
 function computeFigure()
 {
-  console.log("Equals sign pressed")
+  console.log("Equals sign (or log) pressed")
   arg2 = buffer
   let finalOutput = 0
   bComputedNumberOnScreen = true
@@ -162,6 +200,20 @@ function computeFigure()
       outputToScreen(finalOutput)
       flushTempValues()
       break;
+    case "**":
+      console.log(arg1, arg2)
+      finalOutput = Math.pow(arg1, arg2)
+      arg1 = finalOutput
+      console.log(`final output ${finalOutput}`)
+      outputToScreen(finalOutput)
+      flushTempValues()
+    case "log":
+      console.log(arg1)
+      finalOutput = math.round(getBaseLog(arg1),5)
+      arg1 = finalOutput
+      console.log(`final output ${finalOutput}`)
+      outputToScreen(finalOutput)
+      flushTempValues()
   }
   // value = 88765
   arg1 = finalOutput
@@ -183,11 +235,6 @@ function outputToScreen(value)
 
 }
 
-
-function doubleZeroClicked()
-{
-  console.log(0, 0)
-}
 
 function addXNumbers(...zargs)
 {
@@ -244,4 +291,19 @@ function flushTempValues()
   bDecimalPointJustClicked = false
   bDecimalPointInBuffer = false
   currentOp = null
+  bNegativeNumber = false
+}
+
+rr.counter = 0
+function rr()
+{
+  rr.counter++
+  if (rr.counter > 2)
+  {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+  }
+}
+
+function getBaseLog(x, y=2.71828){
+  return Math.log(x) / Math.log(y)
 }
